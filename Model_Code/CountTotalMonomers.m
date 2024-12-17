3c0ee33f4337ac5d27398d0a4f7231b983a46372
Remi Sondaz <141277859+remisondaz@users.noreply.github.com>
2024-12-17 14:43:27 -0700

    Delete Model_Code directory
    Signed-off-by: Remi Sondaz <141277859+remisondaz@users.noreply.github.com>

--- Model_Code/CountTotalMonomers.m
+++
@@ -1,9 +0,0 @@
-function nMonomers = CountTotalMonomers(Filaments)
-
-    nF = length(Filaments.MonomerIndices);
-    nMonomers = 0;
-    for f = 1:nF
-        nMonomers = nMonomers + length(Filaments.MonomerIndices{f});
-    end
-
-end
