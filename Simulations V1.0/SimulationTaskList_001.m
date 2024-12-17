9f5bab73524d2696e524bca6021099b3b6fe701d
Remi Sondaz <141277859+remisondaz@users.noreply.github.com>
2024-12-17 15:22:18 -0700

    Update SimulationTaskList_001.m
    Signed-off-by: Remi Sondaz <141277859+remisondaz@users.noreply.github.com>

--- Simulations V1.0/SimulationTaskList_001.m
+++ Simulations V1.0/SimulationTaskList_001.m
@@ -1,30 +1,28 @@
 % rng('shuffle')  % execute this line if you're adding new simulations after MATLAB was turned off and on.
 clear; 
 clc;
-SaveDirectory = '/Users/remisondaz/Desktop/MATLAB/Extended_sims';    
+SaveDirectory = '/uufs/chpc.utah.edu/common/home/bidone-group3/Remi/Extended_Sims';    
     
-   % Setup all combinations of parameters to be varied  -------------------------------------------------------------------
+     % Setup all combinations of parameters to be varied  -------------------------------------------------------------------
     values = [];
-    nRuns = 3; % Total number of runs for each condition
-    k_a = [0.0001 0.001 0.01 0.1]; % adhesion spring constant
-    peak = [1 2];   % WT or Mn
-    nLigands = 100;
+    %activ = 0.03:0.1:1.5;
+    nRuns = 6; % Total number of runs for each condition
+    k_a = [0.0001 ,0.0001,0.001 0.01 0.1]; % adhesion spring constant
+    peak = [1, 2, 2, 2, 2];   % WT or Mn
+    nLigands = 400;
 
     for m = 1:length(k_a) % Create combinations of all conditions
-        for n = 1:length(peak)
+        p = peak(m);
+        k = k_a(m);
             for r = 1:nRuns
-                for u = 1:length(nLigands)
-                    values = [values; [k_a(m), peak(n), nLigands(u), r] ];
-                end
+                        values = [values; [k, p, nLigands, r] ];
             end
-        end
     end
     % ---------------------------------------------------------------------------------------------------------------------
     
     
     %for k = 1:size(values,1)
-    parfor k = 1:size(values,1) 
-        tic 
+     parfor k = 1:size(values,1) 
             ModelParameters = InitializeModelParameters;  % Initialize Default Model paramaeters
             ModelParameters.TimeStep = 1e-4;
             % Special Conditions (or edits to ModelParameters) ==============================================================
@@ -38,18 +36,18 @@ SaveDirectory = '/Users/remisondaz/Desktop/MATLAB/Extended_sims';
                 ModelParameters.k_branch = 2.2; % s^-1
                 ModelParameters.FAL_connection_Distance = 10*2.75; % nm
                 ModelParameters.MaximumFilamentMass = 4000; % monomers
+                %ModelParameters.Adhesion_ActivationRate = values(k,5); %Varying adhesion activation rate
                 ModelParameters.MolecularClutch_PeakNumber = values(k,2); % Catch bond (1 = WildType or 2 = Manganese)
             % ===============================================================================================================
     
-            SaveName = ['SIMULATION-001__','Ka_',SimFormat(values(k,1)),'__Peak_',sprintf('%02d',values(k,2)),'__nLigands_',sprintf('%04d',values(k,3)),'__run_',sprintf('%02d',values(k,4)),'.mat'];
+            SaveName = ['SIMULATION-001__','Ka_',SimFormat(values(k,1)),'__Peak_',sprintf('%02d',values(k,2)),'__nLigands_',sprintf('%04d',values(k,3)),'_run_',sprintf('%02d',values(k,4)),'.mat'];
         
             if ~isfile(fullfile(SaveDirectory,SaveName))  % If code is re-run it will only run values for files not created/completedsimulation yet (so be sure to delete old runs in the same folder)
-                 disp(SaveName);
+            %     disp(SaveName);
                 LamellipodiumModel_Bidone01(ModelParameters,SaveDirectory,SaveName); % Run simulation
             end
     end
-     disp('SIMULATION-001 complete') 
-     toc
+    % disp('SIMULATION-001 complete') 
 
     quit 
     
