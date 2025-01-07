93e8be97edd666adc507ef9be3ccb07a4d51d585
Remi Sondaz <141277859+remisondaz@users.noreply.github.com>
2025-01-07 14:42:28 -0700

    Update InitializeFALconnections.m
    Signed-off-by: Remi Sondaz <141277859+remisondaz@users.noreply.github.com>

--- Simulations V1.0/InitializeFALconnections.m
+++ Simulations V1.0/InitializeFALconnections.m
@@ -1,6 +1,6 @@
 function FALconnections = InitializeFALconnections
 
-     FALconnections.AdhesionIndex = zeros(0,1);
+     FALconnections.IntegrinIndex = zeros(0,1);
      FALconnections.LigandIndex   = zeros(0,1);
      FALconnections.FilamentName  = zeros(0,1);
      FALconnections.MonomerIndex  = zeros(0,1);
