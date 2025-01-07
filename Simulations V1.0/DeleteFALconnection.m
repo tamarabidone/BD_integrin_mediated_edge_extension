282a561691d88343ea4b1af0dbd8a5543e329ac7
Remi Sondaz <141277859+remisondaz@users.noreply.github.com>
2025-01-07 14:45:45 -0700

    Update DeleteFALconnection.m
    Signed-off-by: Remi Sondaz <141277859+remisondaz@users.noreply.github.com>

--- Simulations V1.0/DeleteFALconnection.m
+++ Simulations V1.0/DeleteFALconnection.m
@@ -1,4 +1,4 @@
-function [Filaments,Adhesions,Ligands,FALconnections] = DeleteFALconnection(Field,Value,Filaments,Adhesions,Ligands,FALconnections)
+function [Filaments,Integrins,Ligands,FALconnections] = DeleteFALconnection(Field,Value,Filaments,Integrins,Ligands,FALconnections)
 
 
             switch Field
@@ -6,16 +6,16 @@ function [Filaments,Adhesions,Ligands,FALconnections] = DeleteFALconnection(Fiel
                     case 'FilamentName'  % Value = FilamentName
                             FALidx = find( FALconnections.FilamentName == Value ); 
                             if ~isempty(FALidx) % If this filament has a connection
-                                 a = FALconnections.AdhesionIndex(FALidx,1);
-                                 Adhesions.AttachedFilamentName(a,1) = NaN;
-                                 Adhesions.AttachcedLigandIndex(a,1) = NaN;
-                                 Adhesions.ActiveStatus(a,1) = false;
+                                 a = FALconnections.IntegrinIndex(FALidx,1);
+                                 Integrins.AttachedFilamentName(a,1) = NaN;
+                                 Integrins.AttachcedLigandIndex(a,1) = NaN;
+                                 Integrins.ActiveStatus(a,1) = false;
 
                                  l = FALconnections.LigandIndex(FALidx,1);
                                  Ligands.AttachedFilamentName(l,1)  = NaN;
-                                 Ligands.AttachedAdhesionIndex(l,1) = NaN;
+                                 Ligands.AttachedIntegrinIndex(l,1) = NaN;
 
-                                 FALconnections.AdhesionIndex(FALidx,:) = [];
+                                 FALconnections.IntegrinIndex(FALidx,:) = [];
                                  FALconnections.FilamentName (FALidx,:) = [];
                                  FALconnections.MonomerIndex (FALidx,:) = [];
                                  FALconnections.LigandIndex  (FALidx,:) = [];
@@ -24,20 +24,20 @@ function [Filaments,Adhesions,Ligands,FALconnections] = DeleteFALconnection(Fiel
 
                     %...................................................................................
                     case 'FilamentNameAndMonomerIndex' % Needs to inputs for value: Value = [ FilamentName, MonomerIndex ] 
-                            FALidx = find( FALconnections.FilamentName == Value(1) & ... % Find the adhesion/ligand/filamement connection with this filament name
-                                           FALconnections.MonomerIndex == Value(2) );    % and the adhesion/ligand that is connected to the current monomer being depolymerized
-                                % If so, reset adhesion/ligand, and remove connection  
+                            FALidx = find( FALconnections.FilamentName == Value(1) & ... % Find the integrin/ligand/filamement connection with this filament name
+                                           FALconnections.MonomerIndex == Value(2) );    % and the integrin/ligand that is connected to the current monomer being depolymerized
+                                % If so, reset integrin/ligand, and remove connection  
                                 if ~isempty(FALidx) 
-                                    a = FALconnections.AdhesionIndex(FALidx,1);
-                                    Adhesions.AttachedFilamentName (a,1) = NaN;
-                                    Adhesions.AttachedLigandIndex  (a,1) = NaN;
-                                    Adhesions.ActiveStatus         (a,1) = false;
+                                    a = FALconnections.IntegrinIndex(FALidx,1);
+                                    Integrins.AttachedFilamentName (a,1) = NaN;
+                                    Integrins.AttachedLigandIndex  (a,1) = NaN;
+                                    Integrins.ActiveStatus         (a,1) = false;
 
                                     l = FALconnections.LigandIndex(FALidx);
                                     Ligands.AttachedFilamentName (l) = NaN;
-                                    Ligands.AttachedAdhesionIndex(l) = NaN;
+                                    Ligands.AttachedIntegrinIndex(l) = NaN;
 
-                                    FALconnections.AdhesionIndex(FALidx) = [];   
+                                    FALconnections.IntegrinIndex(FALidx) = [];   
                                     FALconnections.LigandIndex  (FALidx) = [];
                                     FALconnections.FilamentName (FALidx) = []; 
                                     FALconnections.MonomerIndex (FALidx) = []; 
@@ -45,21 +45,21 @@ function [Filaments,Adhesions,Ligands,FALconnections] = DeleteFALconnection(Fiel
 
 
                     %...................................................................................
-                    case 'AdhesionIndex'  % Value = AdhesionIndex
+                    case 'IntegrinIndex'  % Value = IntegrinIndex
                                 
                                 for V = Value'
-                                        FALidx = find( FALconnections.AdhesionIndex == V );
+                                        FALidx = find( FALconnections.IntegrinIndex == V );
                                         if ~isempty(FALidx) 
                                             a = V;
-                                            Adhesions.AttachedFilamentName(a,1) = NaN;
-                                            Adhesions.AttachedLigandIndex (a,1) = NaN;
-                                            Adhesions.ActiveStatus        (a,1) = false;
+                                            Integrins.AttachedFilamentName(a,1) = NaN;
+                                            Integrins.AttachedLigandIndex (a,1) = NaN;
+                                            Integrins.ActiveStatus        (a,1) = false;
 
                                             l = FALconnections.LigandIndex(FALidx);
                                             Ligands.AttachedFilamentName (l) = NaN;
-                                            Ligands.AttachedAdhesionIndex(l) = NaN;
+                                            Ligands.AttachedIntegrinIndex(l) = NaN;
 
-                                            FALconnections.AdhesionIndex(FALidx) = [];   
+                                            FALconnections.IntegrinIndex(FALidx) = [];   
                                             FALconnections.LigandIndex  (FALidx) = [];
                                             FALconnections.FilamentName (FALidx) = []; 
                                             FALconnections.MonomerIndex (FALidx) = []; 
@@ -71,16 +71,16 @@ function [Filaments,Adhesions,Ligands,FALconnections] = DeleteFALconnection(Fiel
                     case 'LigandIndex' % Value = LigandIndex
                             FALidx = find( FALconnections.LigandIndex == Value );
                             if ~isempty(FALidx) 
-                                a = FALconnections.AdhesionIndex(FALidx,1);
-                                Adhesions.AttachedFilamentName(a,1) = NaN;
-                                Adhesions.AttachedLigandIndex (a,1) = NaN;
-                                Adhesions.ActiveStatus        (a,1) = false;
+                                a = FALconnections.IntegrinIndex(FALidx,1);
+                                Integrins.AttachedFilamentName(a,1) = NaN;
+                                Integrins.AttachedLigandIndex (a,1) = NaN;
+                                Integrins.ActiveStatus        (a,1) = false;
 
                                 l = FALconnections.LigandIndex(FALidx);
                                 Ligands.AttachedFilamentName (l) = NaN;
-                                Ligands.AttachedAdhesionIndex(l) = NaN;
+                                Ligands.AttachedIntegrinIndex(l) = NaN;
 
-                                FALconnections.AdhesionIndex(FALidx) = [];   
+                                FALconnections.IntegrinIndex(FALidx) = [];   
                                 FALconnections.LigandIndex  (FALidx) = [];
                                 FALconnections.FilamentName (FALidx) = []; 
                                 FALconnections.MonomerIndex (FALidx) = []; 
