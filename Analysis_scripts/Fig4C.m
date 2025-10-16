b25861811618c17f2983c568a9175d5af3693925
Remi Sondaz <141277859+remisondaz@users.noreply.github.com>
2025-10-16 10:45:19 -0600

    Update Fig4C.m
    Signed-off-by: Remi Sondaz <141277859+remisondaz@users.noreply.github.com>

--- Analysis_scripts/Fig4C.m
+++ Analysis_scripts/Fig4C.m
@@ -52,49 +52,30 @@ nligands = 400;
   data11 = load("Nematic_vals.k_a_001d0000_peak_1_nligands_400.mat");
   data12 = load("Nematic_vals.k_a_001d0000_peak_2_nligands_400.mat");
 
-  
-  nem = {unique(data1.Nem_order), unique(data2.Nem_order), data3.Nem_order, ...,
-  data4.Nem_order, data5.Nem_order, unique(data6.Nem_order),  ...
-  data7.Nem_order, unique(data8.Nem_order),data9.Nem_order,unique(data10.Nem_order),...
-  unique(data11.Nem_order),unique(data12.Nem_order)};
-  [Fout,nCells,Ns] = CellArray2PaddedNanArray(nem');
-Fout = Fout(:, [3,4,5,7]);
-FH = figure(1); clf
-
-% subplot(1,3,3)
-FH.Color = 'w';
-MarkerSize = 20;
- bh = violinplot(Fout);
-colors = {[0, 0, 0], [1, 0.75, 0.796], [0.678, 0.847, 0.902], [0.502, 0, 0.502]};  % Example colors
-% Loop through each violin and set the color
-for f = 1:length(bh)
-    bh(f).ViolinColor = colors(f);
-    bh(f).ShowData = false;
-    bh(f).ShowMedian = true;
-    bh(f).MedianColor = [1 0.5 0];
-    bh(f).ShowBox = true;
-    bh(f).ShowMean = false;
-    bh(f).ShowNotches = false;
-    bh(f).EdgeColor = [0 0 0];
-    bh(f).ShowWhiskers = true;
-    %bh(i).QuartileStyle = 'shadow';
-    %bh(i).MedianMarkerSize = 48;
-    %bh(i).ViolinAlpha = 1;
-end
-FS = 45;
- %bh.LineWidth = 3;
-%ylim([0 1])
-set(gca, 'box', 'off', 'fontsize', FS, 'LineWidth', 3)
-set(gca, 'TickLabelInterpreter', 'latex','FontName', 'Arial', 'FontSize', FS);
-xticks([1 2 3 4])
-%xticklabels({'0.4', '0.4\\newline$+ Mn^{2+}$', '6', '60'});
-xticklabels({' ', ' ', ' ', ' '});
-%set(gca, 'TickLabelInterpreter', 'none');  % Disable LaTeX interpretation for xticklabels
-xlabel('Substrate rigidity (kPa)','FontSize', FS);
-ylabel('Order Parameter', 'FontSize',FS);
-set(gcf, 'Position',[700 100 700 1000]);
-exportgraphics(gca,'ExtendedSims2.png','Resolution',450)
-
+% figure; 
+% hold on;
+% set(gcf,'Color','w')
+% 
+% bh = violinplot(data);
+% 
+% for f = 1:length(bh)
+%     bh(f).ViolinColor = colors(f);
+%     bh(f).ShowData = true;
+%     bh(f).ShowMedian = true;
+%     bh(f).MedianColor = [1 0.5 0];
+%     bh(f).ShowBox = true;
+%     bh(f).ShowMean = false;
+%     bh(f).ShowNotches = false;
+%     bh(f).EdgeColor = [0 0 0];
+%     bh(f).ShowWhiskers = true;
+%     %bh(i).QuartileStyle = 'shadow';
+%     %bh(i).MedianMarkerSize = 48;
+%     %bh(i).ViolinAlpha = 1;
+% end
+% set(gca, 'box', 'off', 'fontsize', 25, 'LineWidth', 3)
+% set(gca, 'TickLabelInterpreter', 'latex','FontName', 'Times', 'FontSize', 25);
+% box off
+% hold off
 
 
 
